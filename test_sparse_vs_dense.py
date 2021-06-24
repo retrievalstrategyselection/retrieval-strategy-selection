9098c520089c387e8930abf3f9a13c9df072dc37
retrievalstrategyselection <85660162+retrievalstrategyselection@users.noreply.github.com>
2021-06-24 13:22:55 -0400

    Update test_sparse_vs_dense.py

--- test_sparse_vs_dense.py
+++ test_sparse_vs_dense.py
@@ -9,8 +9,8 @@ import math,logging
 
 batch_size=32
 
-
-model = CrossEncoder('models/sparse_vs_dense_model_bert-base-uncased_1_b32', num_labels=2)
+model_name="sparse_vs_dense_bert-base-uncased_e2_b32"
+model = CrossEncoder('models/'+model_name, num_labels=2)
 sentences = []
 qs=[]
 
@@ -23,7 +23,7 @@ for line in labels:
 
 scores=model.predict(sentences)
 actual=[]
-out=open('results/prediction_sparse_vs_dense.dev.small.tsv','w')
+out=open('results/prediction_'+model_name+'.dev.small.tsv','w')
 out.write('qid\tquery\tsparse_prob\tdense_prob\n')
 for i in range(len(sentences)):
     out.write(qs[i]+'\t'+sentences[i][0]+'\t'+str(scores[i][0])+'\t'+str(scores[i][1])+'\n')
